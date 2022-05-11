00969fc0dc140fa2d57682487b09cc4227eb602e
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-05-11 16:14:44 +0500

    ergfer

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
-var elChiqar =$_(".chiqar-js");
+let elChiqar =$_(".chiqar-js");
 
 
-for (var i = 0; i < pokemons.length; i++) {
-	var list = document.createElement("li");
-list.setAttribute("class", "col-sm-4 col-md-3 col-lg-2 text-center p-3 mx-2 mb-2 border border-2 border-success rounded bg-light");
+for (let i = 0; i < pokemons.length; i++) {
+	let list = document.createElement("li");
+	list.setAttribute("class", "col-sm-4 col-md-3 col-lg-2 text-center p-3 mx-2 mb-2 border border-2 border-success rounded bg-light");
 
-var image = document.createElement("img");
-image.src = pokemons[i].img;
-list.append(image);
+	let image = document.createElement("img");
+	image.src = pokemons[i].img;
+	list.append(image);
 
-var ism = document.createElement("h4");
-ism.textContent = pokemons[i].name;
-list.append(ism);
+	let ism = document.createElement("h4");
+	ism.textContent = pokemons[i].name;
+	list.append(ism);
 
 
-var turi = document.createElement("p");
-turi = pokemons[i].type;
+	let turi = document.createElement("p");
+	turi = pokemons[i].type;
 
-list.append(turi);
-elChiqar.append(list);
+	list.append(turi);
+	elChiqar.append(list);
 
-console.log(pokemons);
+	console.log(pokemons);
 }
-	
