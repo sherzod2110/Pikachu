8613495a00096c2bf8080d9adea8c857a7835887
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-05-12 09:57:29 +0500

    sdfgwegew

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let elList = $_(".js-list");
 
-
 pokemons.forEach(function (pokemon) {
 	let newLiItem = createElement('li', 'list-item',);
 	let newImg = createElement('img', 'img img-fluid');
@@ -9,11 +8,7 @@ pokemons.forEach(function (pokemon) {
 	let newLiName = createElement('p', 'item-name', pokemon.name.toUpperCase());
 	let newLiType = createElement('p', 'item-span', pokemon.type);
 
-	
-	newLiItem.appendChild(newImg);
-	newLiItem.appendChild(newLiName);
-	newLiItem.appendChild(newLiType);
-	
+	newLiItem.append(newImg, newLiName, newLiType);
 	elList.appendChild(newLiItem);
 });
 
